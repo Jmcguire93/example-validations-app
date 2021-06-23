0577945035022fa6dff16a2a250a2130e3be578c
James <james.ryan.mcguire@gmail.com>
2021-06-23 15:00:16 -0700

    Adds valid_email method and validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
   validates :first_name, :last_name, presence: true
+  validate :valid_email
+
+  def valid_email(user)
+    if email[-4..-1] != ".com"
+      errors.add(:email "must end in .com")
+    elsif !user.email.include?("@")
+      user.errors.add(:email, "must contain @ symbol.")
+    end
+  end
 end
