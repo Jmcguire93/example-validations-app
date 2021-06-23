6d5e5578ed1d20f01dcbed03446077bb63f8eacd
James <james.ryan.mcguire@gmail.com>
2021-06-23 14:17:29 -0700

    Adds index, show, create and update routes and controller methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,58 @@
 class UsersController < ApplicationController
+
+  def index
+    users = User.all 
+    render json: users.as_json 
+  end
+
+  def show
+    user_id = params["id"]
+    user = User.find_by(id: user_id) 
+    render json: user.as_json
+  end
+
+  def create
+    user = User.new(
+      first_first_name: params["first_name"],
+      last_name: params["last_name"],
+      email: params["email"]
+    )
+
+    if user.save
+      render json: user.as_json
+    else
+      render json: { errors: user.error.full_messages },
+      status: :unprocessable_entity
+    end 
+  end
+
+  def update
+    user_id = params["id"]
+    user = User.find_by(id: user_id)
+
+    user.first_name = params["first_name"] || user.first_name
+    user.last_name = params["last_name"] || user.last_name
+    user.email = params["email"] || user.email
+    
+    if user.save 
+      render json: user.as_json
+    else
+      render json: { errors: user.error.full_messages },
+      status: :unprocessable_entity
+    end
+  end
+
+
+
+  def destroy
+    user_id = params[:id]
+    user = User.find_by(id: user_id)
+    
+    if user.destroy
+      render json: {message: "User sucesfully destroyed."}
+    else
+      render json: { errors: user.error.full_messages },
+      status: :unprocessable_entity
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get "/users", controller: "users", action: "index"    ## Displays a list of all instances 
+
+  get "/users/:id", controller: "users", action: "show"     ## Displays a specific instance
   
+  post "/users", controller: "users", action: "create"    ## creates instances
+
+  patch "/users/:id", controller: "users", action: "update"   ## updates instances
+
+  delete "/users/:id", controller: "users", action: "destroy"  ##deletes a specific product/instance
 end
